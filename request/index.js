c5a83a18ba7ab1d26f25224039a7b482ccc79090
Junmin Ahn <junminahn@outlook.com>
2021-08-12 17:10:22 -0700

    feat: add action for request workflow

---
+++ request/index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const { context, getOctokit } = require('@actions/github');
+const run = require('./run');
+
+process.on('unhandledRejection', handleError);
+main().catch(handleError);
+
+async function main() {
+  const token = core.getInput('github-token', { required: true });
+  const apiUrl = core.getInput('api-url', { required: true });
+  const authSecret = core.getInput('auth-secret', { required: true });
+  const github = getOctokit(token);
+  const result = await run({
+    github: github.rest,
+    context,
+    args: { apiUrl, authSecret },
+  });
+  core.setOutput('result', JSON.stringify(result));
+}
+
+function handleError(err) {
+  console.error(err);
+  core.setFailed(`Unhandled error: ${err}`);
+}
